4205a507673c0641a0713ac98493cd8337404543
Lisa Rozo <erozo190@gmail.com>
2021-10-02 10:54:34 -0500

    added index

--- index.js
+++ index.js
@@ -41,3 +41,18 @@ This is a short description of this application: ${obj.description}
 }
 
 askQuestions()
+
+
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
